76699e4d9848dd9e8a9fe64214e6a095508f60dd
alen <alen.ankajcan@gmail.com>
2019-08-27 20:25:18 +0200

    Add products soft deletes

--- database/migrations/2019_07_08_115454_create_products_table.php
+++ database/migrations/2019_07_08_115454_create_products_table.php
@@ -22,6 +22,7 @@ class CreateProductsTable extends Migration
             $table->decimal('discount_price')->nullable();
             $table->boolean('featured')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
