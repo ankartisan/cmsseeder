dcc550c59de3521e22d07c03403f811ece525cca
alen <alen.ankajcan@gmail.com>
2019-10-20 00:18:06 +0200

    fix migration tables

--- database/migrations/2019_10_01_192905_create_product_attribute_options_table.php
+++ database/migrations/2019_10_01_192905_create_product_attribute_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductVariantOptionsTable extends Migration
+class CreateProductAttributeOptionsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2019_10_01_201848_create_product_attribute_option_combinations.php
+++ database/migrations/2019_10_01_201848_create_product_attribute_option_combinations.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductVariantOptionCombinations extends Migration
+class CreateProductAttributeOptionCombinations extends Migration
 {
     /**
      * Run the migrations.
